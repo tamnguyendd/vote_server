17daacff59a97f6b11afb9619c73a22e29781159
tamncrikkei <tamnc@rikkeisoft.com>
2022-09-13 23:42:36 +0900

    no message

--- database/db_connect.js
+++ database/db_connect.js
@@ -6,6 +6,6 @@ const pgClient = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.PORT,
+  //port: process.env.PORT,
 });
 module.exports = pgClient;
